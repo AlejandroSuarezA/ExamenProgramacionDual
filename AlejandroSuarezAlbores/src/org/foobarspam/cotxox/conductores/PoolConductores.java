02df0cda03fca0dea2956a809b494e8d401e06c2
Jandro <jandro.suarez.92@gmail.com>
2017-02-16 19:21:37 +0100

    Testeado el metodo que comprueba si el conductor esta libre

--- AlejandroSuarezAlbores/src/org/foobarspam/cotxox/conductores/PoolConductores.java
+++ AlejandroSuarezAlbores/src/org/foobarspam/cotxox/conductores/PoolConductores.java
@@ -1,5 +1,33 @@
 package org.foobarspam.cotxox.conductores;
 
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class PoolConductores {
+	
+	private ArrayList<Conductor> poolConductores = new ArrayList<>();
+	
+	
+	public PoolConductores(ArrayList<Conductor> pool){
+		
+		this.poolConductores = pool;
+		
+	}
+	
+	public Conductor asignarConductor(){
+		int pilotoNumero = ThreadLocalRandom.current().nextInt(0, this.poolConductores.size());
+		Conductor pilotoAsignado = null;
+		
+		while(pilotoAsignado == null){
+			if(this.poolConductores.get(pilotoNumero).isOcupado() == true){
+				pilotoAsignado = this.poolConductores.get(pilotoNumero);
+			}
+			else{
+				pilotoNumero = ThreadLocalRandom.current().nextInt(0, this.poolConductores.size());
+			}
+		}
+		return pilotoAsignado;
+	}
+	
 
 }
