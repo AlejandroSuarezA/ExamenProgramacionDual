e885e984e6887607f4d27c8fb74c586b335c85d9
Jandro <jandro.suarez.92@gmail.com>
2017-02-16 20:11:44 +0100

    Codigo reordenado y preparado para entregar. Finito.

--- AlejandroSuarezAlbores/src/org/foobarspam/cotxox/tarifa/Tarifa.java
+++ AlejandroSuarezAlbores/src/org/foobarspam/cotxox/tarifa/Tarifa.java
@@ -1,44 +1,45 @@
 package org.foobarspam.cotxox.tarifa;
 
 public class Tarifa {
+	// Attributes
+
 	private static double costeMilla = 1.35;
 	private static double costeMinuto = 0.35;
 	private static double costeMinimo = 5.00;
-	
-	
-	public static double getCosteMilla(){
+
+	// Sets & Gets
+
+	public static double getCosteMilla() {
 		return costeMilla;
 	}
 
-	
-	public static double getCosteDistancia(double distancia){
-		return getCosteMilla() * distancia ;
-		
-	}
-	
-	public static double getCosteMinuto(){
+	public static double getCosteMinuto() {
 		return costeMinuto;
 	}
-	
-	public static double getCosteMinimo(){
+
+	public static double getCosteMinimo() {
 		return costeMinimo;
 	}
 
-	
-	public static double getCosteTiempo(double tiempo){
+	// Price calculators
+
+	public static double getCosteDistancia(double distancia) {
+		return getCosteMilla() * distancia;
+
+	}
+
+	public static double getCosteTiempo(double tiempo) {
 		return getCosteMinuto() * tiempo;
 	}
-	
-	public static double getCosteTotalEsperado(double tiempo, double distancia){
+
+	public static double getCosteTotalEsperado(double tiempo, double distancia) {
 		double precioCalculado = getCosteTiempo(tiempo) + getCosteDistancia(distancia);
-		
-		if (precioCalculado >= getCosteMinimo()){
+
+		if (precioCalculado >= getCosteMinimo()) {
 			return precioCalculado;
-		}
-		else{
+		} else {
 			return getCosteMinimo();
 		}
 	}
-	
 
 }
