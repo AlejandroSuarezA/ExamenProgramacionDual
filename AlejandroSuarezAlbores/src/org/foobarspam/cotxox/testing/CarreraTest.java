e5857cf8077f06be2d5163a27d41dd338d582ebd
Jandro <jandro.suarez.92@gmail.com>
2017-03-02 13:23:04 +0100

    Before creado en los test de Carrera

--- AlejandroSuarezAlbores/src/org/foobarspam/cotxox/testing/CarreraTest.java
+++ AlejandroSuarezAlbores/src/org/foobarspam/cotxox/testing/CarreraTest.java
@@ -4,25 +4,41 @@ import org.foobarspam.cotxox.carrera.Carrera;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CarreraTest {
-
-
+	String tarjetaCredito;
+	String origen;
+	String destino;
+	double distancia;
+	int tiempoEsperadoMinutos;
+	Carrera carrera;
 	
-	@Test
-	public void testOrigen() {
+	@Before
+	public void intialize(){
 		String tarjetaCredito = "4916119711304546";
 		String origen = "Aeroport Son Sant Joan";
 		String destino= "Magaluf";
 		double distancia = 7.75;
 		int tiempoEsperadoMinutos = 10;
 		
-		Carrera carrera = new Carrera(tarjetaCredito);
+		
+		carrera = new Carrera(tarjetaCredito);
 		carrera.setOrigen(origen);
 		carrera.setDestino(destino);
 		carrera.setDistancia(distancia);
 		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+		
+	}
+
+	
+
+	
+	@Test
+	public void testOrigen() {
+
+		origen = "Aeroport Son Sant Joan";
 		
 		assertEquals(origen, carrera.getOrigen());
 		
@@ -30,68 +46,31 @@ public class CarreraTest {
 	
 	@Test
 	public void testDestino(){
-		String tarjetaCredito = "4916119711304546";
-		String origen = "Aeroport Son Sant Joan";
-		String destino= "Magaluf";
-		double distancia = 7.75;
-		int tiempoEsperadoMinutos = 10;
-		
-		Carrera carrera = new Carrera(tarjetaCredito);
-		carrera.setOrigen(origen);
-		carrera.setDestino(destino);
-		carrera.setDistancia(distancia);
-		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+
+		destino = "Magaluf";
 		
 		assertEquals(destino, carrera.getDestino());
 	}
 	
 	@Test
 	public void testTarjetaCredito(){
-		String tarjetaCredito = "4916119711304546";
-		String origen = "Aeroport Son Sant Joan";
-		String destino= "Magaluf";
-		double distancia = 7.75;
-		int tiempoEsperadoMinutos = 10;
-		
-		Carrera carrera = new Carrera(tarjetaCredito);
-		carrera.setOrigen(origen);
-		carrera.setDestino(destino);
-		carrera.setDistancia(distancia);
-		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+
+		tarjetaCredito = "4916119711304546";
 		
 		assertEquals(tarjetaCredito, carrera.getTarjetaCredito());
 	}
 	
 	@Test
 	public void testDistancia(){
-		String tarjetaCredito = "4916119711304546";
-		String origen = "Aeroport Son Sant Joan";
-		String destino= "Magaluf";
-		double distancia = 7.75;
-		int tiempoEsperadoMinutos = 10;
-		
-		Carrera carrera = new Carrera(tarjetaCredito);
-		carrera.setOrigen(origen);
-		carrera.setDestino(destino);
-		carrera.setDistancia(distancia);
-		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+
+		distancia = 7.75;
 		
 		assertEquals(distancia, carrera.getDistancia(), 0.0);
 	}
 	
 	@Test
 	public void testPrecio(){
-		String tarjetaCredito = "4916119711304546";
-		String origen = "Aeroport Son Sant Joan";
-		String destino= "Magaluf";
-		double distancia = 7.75;
-		int tiempoEsperadoMinutos = 10;
-		
-		Carrera carrera = new Carrera(tarjetaCredito);
-		carrera.setOrigen(origen);
-		carrera.setDestino(destino);
-		carrera.setDistancia(distancia);
-		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+
 		
 		double precioEsperado = 13.9625;
 		
@@ -102,15 +81,10 @@ public class CarreraTest {
 	@Test
 	public void testPrecioMinimo(){
 		
-		String tarjetaCredito = "4916119711304546";
-		String origen = "Aeroport Son Sant Joan";
-		String destino= "Magaluf";
+
 		double distancia = 1.00;
 		int tiempoEsperadoMinutos = 1;
-		
-		Carrera carrera = new Carrera(tarjetaCredito);
-		carrera.setOrigen(origen);
-		carrera.setDestino(destino);
+
 		carrera.setDistancia(distancia);
 		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
 		
@@ -123,17 +97,9 @@ public class CarreraTest {
 	
 	@Test
 	public void testPagoRealizado(){
-		String tarjetaCredito = "4916119711304546";
-		String origen = "Aeroport Son Sant Joan";
-		String destino= "Magaluf";
-		double distancia = 7.75;
-		int tiempoEsperadoMinutos = 10;
+
 		
-		Carrera carrera = new Carrera(tarjetaCredito);
-		carrera.setOrigen(origen);
-		carrera.setDestino(destino);
-		carrera.setDistancia(distancia);
-		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+
 		
 		double precioEsperado = 13.9625;
 		carrera.realizarPago(carrera.getCosteEsperado());
@@ -145,17 +111,7 @@ public class CarreraTest {
 	
 	@Test
 	public void testPropina(){
-		String tarjetaCredito = "4916119711304546";
-		String origen = "Aeroport Son Sant Joan";
-		String destino= "Magaluf";
-		double distancia = 7.75;
-		int tiempoEsperadoMinutos = 10;
-		
-		Carrera carrera = new Carrera(tarjetaCredito);
-		carrera.setOrigen(origen);
-		carrera.setDestino(destino);
-		carrera.setDistancia(distancia);
-		carrera.setTiempoEsperado(tiempoEsperadoMinutos);
+
 		
 		double precioEsperado = 5;
 		carrera.recibirPropina(5);
